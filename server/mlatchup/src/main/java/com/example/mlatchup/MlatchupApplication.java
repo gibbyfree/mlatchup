be4919edbf4070d609e78b9596c0a02d92c7fc4a
gibbyfree <free.g@husky.neu.edu>
2020-11-21 17:34:16 -0500

    Init commit for server

---
+++ server/mlatchup/src/main/java/com/example/mlatchup/MlatchupApplication.java
@@ -0,0 +1,13 @@
+package com.example.mlatchup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlatchupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlatchupApplication.class, args);
+	}
+
+}
